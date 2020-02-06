52aa6635cf4849fda4e55b2226d8421ab17713a6
Lucas Viana <vianadossantoslucas@gmail.com>
2020-02-05 23:43:57 -0300

    chore(blog): mudança no nome do blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `Blog do baú`,
+    title: `Blog do Lucas`,
     author: `Lucas Viana`,
     description: `Um blog sobre tecnologia, produtividade e cotidiano. Produzido e mantido por Lucas Viana © 2019`,
     social: [
