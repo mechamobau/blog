c1eea082041474d049d88b898032d6cd8acb0e39
Lucas Viana <vianadossantoslucas@gmail.com>
2020-06-13 19:49:42 -0300

    chore: updates bio content

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,10 +1,12 @@
 import React from "react"
-import { Styled } from "theme-ui"
 
 /**
  * Change the content to add your own bio
  */
 
 export default () => (
-  <Styled.p>Amo aprender, ensinar e conversar sobre tecnologia.</Styled.p>
+  <>
+    Me chamo Lucas Viana, mas pode me chamar de Baú! Amo aprender, ensinar e
+    conversar sobre tecnologia.
+  </>
 )
